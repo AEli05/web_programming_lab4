0c1da4d494075e794fee19004c5d1e97d48708e9
Anastasia <3540455@bk.ru>
2025-12-25 23:14:04 +0300

    add weather container and form for the cities

--- js/script.js
+++ js/script.js
@@ -33,3 +33,37 @@ function createRefreshButton(container) {
     return button;
 }
 
+function createWeatherContainer(container) {
+    const weatherContainer = document.createElement("div");
+    weatherContainer.id = "weather-container";
+    container.appendChild(weatherContainer);
+    return weatherContainer;
+}
+
+function createCityForm(container) {
+    const form = document.createElement("form");
+    form.classList.add("city-form");
+
+    const input = document.createElement("input");
+    input.type = "text";
+    input.placeholder = "Введите город:";
+    input.id = "city-input";
+
+    const suggestions = document.createElement("ul");
+    suggestions.classList.add("suggestions");
+    suggestions.id = "suggestions-list";
+
+    const error = document.createElement("div");
+    error.classList.add("error-message");
+    error.id = "city-error";
+    error.style.display = "none";
+
+    form.append(input, suggestions, error);
+    container.appendChild(form);
+
+    return {form, input, suggestions, error};
+
+}
+
+
+
