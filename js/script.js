dac77d3551ba78eec823095d04df3908b7aa1f5d
Anastasia <3540455@bk.ru>
2025-12-25 22:56:11 +0300

    add h1 sent and update button

--- js/script.js
+++ js/script.js
@@ -13,3 +13,23 @@ function createAppContainer() {
     document.body.appendChild(appContainer);
     return appContainer;
 }
+
+function createTile(container) {
+    const tile = document.createElement("h1");
+    tile.textContent = "Прогноз погоды";
+    tile.classList.add("app-tile");
+    container.appendChild(tile)
+}
+
+function createRefreshButton(container) {
+    const button = document.createElement("button");
+    button.id = "refresh-btn";
+    button.textContent = "Обновить";
+    button.disabled = true;
+    button.addEventListener("click", () => {
+        console.log("проверка 2.0");
+    });
+    container.appendChild(button);
+    return button;
+}
+
