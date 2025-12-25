c922295a1429e0f5037b7f17b562d6eb846de64a
Anastasia <3540455@bk.ru>
2025-12-25 23:51:29 +0300

    add init block, error message, cityform

--- js/script.js
+++ js/script.js
@@ -6,6 +6,11 @@ let currentState = {
     cities: [],
 };
 let appContainer = null;
+let cityInput = null;
+let suggestionsList = null;
+let cityError = null;
+let refreshButton = null;
+let weatherContainer = null;
 
 function createAppContainer() {
     appContainer = document.createElement("div");
@@ -43,11 +48,14 @@ function createWeatherContainer(container) {
 function createCityForm(container) {
     const form = document.createElement("form");
     form.classList.add("city-form");
+    form.id = "city_form";
+    form.style.display = "none";
 
     const input = document.createElement("input");
     input.type = "text";
     input.placeholder = "Введите город:";
     input.id = "city-input";
+    input.autocomplete = "off";
 
     const suggestions = document.createElement("ul");
     suggestions.classList.add("suggestions");
@@ -65,5 +73,49 @@ function createCityForm(container) {
 
 }
 
+function showError(message) {
+    const error = document.getElementById("city-error");
+    error.textContent = message;
+    error.style.display = "block";
+    setTimeout(() => {
+        error.style.display = "none";
+    }, 5000);
+}
 
+function clearSuggestions() {
+    suggestionsList.innerHTML = "";
+}
+
+function showCityForm() {
+    const cityForm = document.getElementById("city-form");
+    const cityInput = document.getElementById("city-input");
+
+    cityForm.style.display = "block";
+    cityInput.focus();
+    cityInput.value = "";
+    clearSuggestions();
+}
+
+function requestLocation() {
+    if (!navigator.geolocation) {
+        showError("Возникли небольшие проблемы с определением вашей геолокации :(");
+        showCityForm();
+    }
+}
+
+function init() {
+    createAppContainer();
+    createTile(appContainer);
+    weatherContainer = createWeatherContainer(appContainer);
+    const formElements = createCityForm(appContainer);
+
+    cityInput = formElements.input;
+    suggestionsList = formElements.suggestions;
+    cityError = formElements.error;
+    refreshButton = createRefreshButton(appContainer);
+
+    formElements.form.display = "none";
+
+    requestLocation()
+}
 
